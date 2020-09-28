eaf6222b0f66741c273a2b6d71a2d1acd93b273f
Jason Santiago <jasonsantiagobutler@gmail.com>
2020-09-27 21:32:02 -0300

    liftN functions

---
+++ src/pointfree/lift.test.ts
@@ -0,0 +1,23 @@
+import { Right } from '..'
+import { Left } from '../Either'
+import { liftC, lift2C, lift4C } from './lift'
+describe('lift', () => {
+  test('lift2', () => {
+    const add = (a: number) => (b: number) => a + b
+    const addL = lift2C(add)
+    const addFiveOption = addL(Right<number, any>(5))
+    expect(addFiveOption(Right(10))).toEqual(Right(15))
+    expect(addFiveOption(Left('error'))).toEqual(Left('Error'))
+  })
+  test('lift4', () => {
+    const something = lift4C(
+      (a: string) => (b: number) => (c: boolean) => (d: number) => a + b + c + d
+    )
+    expect(something(Right('10'))(Right(10))(Right(true))(Right(10))).toEqual(
+      Right('10' + 10 + true + 10)
+    )
+    expect(
+      something(Right<string, any>('10'))(Left('err'))(Right(true))(Right(10))
+    ).toEqual(Left('err'))
+  })
+})
