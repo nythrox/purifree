20bcedfa9546085010b74876807e16999070c9e2
Jason Santiago <jasonsantiagobutler@gmail.com>
2020-10-15 22:30:59 -0300

    commented possible do notation change

--- src/pointfree/do.ts
+++ src/pointfree/do.ts
@@ -1,6 +1,11 @@
-import { Any } from 'ts-toolbelt'
-import { Chainable } from './chain'
+import { Any, U } from 'ts-toolbelt'
+import { Right } from '../Either'
+import { Just } from '../Maybe'
+import { pipe } from '../utils'
+import { chain, Chainable, chainFlex } from './chain'
+import { filter } from './filter'
 import { of, ReplaceFirst, Type, URIS } from './hkt'
+import { map } from './map'
 export const ofSymbol = Symbol('of')
 export interface GeneratableKind<F extends URIS, A extends any[]>
   extends Chainable<F, A> {
@@ -33,24 +38,6 @@ export function DoFlex<
   return null as any
 }
 
-type Consumer<Value> = (value: Value) => void
-
-type IntersectionFromUnion<Union> = (
-  Union extends infer U ? Consumer<U> : never
-) extends Consumer<infer ResultIntersection>
-  ? ResultIntersection
-  : never
-
-type OverloadedConsumerFromUnion<Union> = IntersectionFromUnion<
-  Union extends infer U ? Consumer<U> : never
->
-
-type UnionLast<Union> = OverloadedConsumerFromUnion<Union> extends (
-  a: infer A
-) => void
-  ? A
-  : never
-
 export function Do<
   R,
   Generatable extends GeneratableKind<any, any>,
@@ -70,7 +57,9 @@ export function Do<
         Rest
       ]
     : () => Generator<Generatable, R, any>
-): MultipleMonadsError extends true ? true : Type<URI, ReplaceFirst<Generatable['_A'], R>> {
+): MultipleMonadsError extends true
+  ? true
+  : Type<URI, ReplaceFirst<Generatable['_A'], R>> {
   const iterator = (fun as () => Generator<Generatable, R, any>)()
   const state = iterator.next()
   const of = (state.value as Generatable)[ofSymbol]
@@ -84,3 +73,15 @@ export function Do<
   }
   return run(state)
 }
+
+// export function Do2<R, URI extends URIS, A extends any[]>(
+//   fun: () => Generator<GeneratableKind<URI, A>, R, any>
+// ): IsUnion<A> extends true
+//   ? { error: 'All secondary generics must be of the same type.' }
+//   : IsUnion<URI> extends true
+//   ? {
+//       error: 'Cannot have monads of different types in do* notation'
+//     }
+//   : Type<URI, ReplaceFirst<A, R>> {
+//   return 0 as any
+// }
