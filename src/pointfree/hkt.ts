902be779ba3f1b57f76b217961f73f71c250426f
Jason Butler <jasonsantiagobutler@gmail.com>
2022-03-20 18:00:24 -0300

    remove ts-toolbelt

--- src/pointfree/hkt.ts
+++ src/pointfree/hkt.ts
@@ -4,35 +4,32 @@ export interface HKT<F extends URIS, A extends any[]> {
   _A: A
 }
 
-// export type TypeFromHKT<
-//   other extends HKT<any, any>,
-//   replaceA extends any[]
-// > = Type<other['_URI'], Replace<other['_A'], replaceA>>
+export type TypeFromHKT<
+  other extends HKT<any, any>,
+  replaceA extends any[]
+  > = Type<other['_URI'], ReplaceUpTo3<other['_A'], replaceA>>
 
-// export type HKTFrom<
-//   other extends HKT<any, any>,
-//   A extends any[],
-//   uri extends URIS = other['_URI'],
-//   oldA extends any[] = other['_A']
-// > = HKT<uri, Replace<oldA, A>>
 
-// export type Replace<oldA extends any[], newA extends any[]> = 
-// // [
-// //   ...L.Merge<newA, oldA>
-// // ]
-// [
-//   ...newA,
-//   ...L.Drop<oldA, L.Length<newA, 's'>, '->'>
-// ]
+type ReplaceUpTo3<O extends any[], R extends any[], L = R['length']> = 
+    L extends 3 ? R : 
+    L extends 2 ? [R[0], R[1], O[2]] : 
+    L extends 1 ? [R[0], O[1], O[2]] : O
+
+export type HKTFrom<
+  other extends HKT<any, any>,
+  A extends any[],
+  uri extends URIS = other['_URI'],
+  oldA extends any[] = other['_A']
+> = HKT<uri, ReplaceUpTo3<oldA, A>>
 
 export type ReplaceFirst<Arr extends any[], T extends any> = Arr extends [
   infer _Head,
   ...infer Rest
 ]
   ? // ? [T, ...Rest]
-    // : Arr
+  // : Arr
 
-    [T, ...Rest]
+  [T, ...Rest]
   : [T, ...any]
 
 export type ReplaceSecond<Arr extends any[], T> = Arr extends [
@@ -63,7 +60,7 @@ export type ReplaceFirstAndSecond<Arr extends any[], A, B> = Arr extends [
 ]
   ? [A, B, ...Rest]
   : // : Arr
-    [A, B]
+  [A, B]
 
 export type OrNever<K> = unknown extends K ? never : K
 export type ProtectFromNever<T> = OrNever<T> extends never ? never : T
@@ -71,7 +68,7 @@ export type ReplaceFirstAndReplaceSecondIfSecondIsNever<
   Arr extends any[],
   A,
   B
-> = Arr extends [infer _First, infer Second, ...infer Rest]
+  > = Arr extends [infer _First, infer Second, ...infer Rest]
   ? [A, OrNever<Second> extends never ? B : Second, ...Rest]
   : [A, B]
 
@@ -88,6 +85,6 @@ export type SwapFirstTwo<Arr extends any[]> = Arr extends [
 
 export type Type<URI extends URIS, A extends any[]> = URI2HKT<A>[URI]
 // Types are sorted by order of priority, not of placement (type #1 is mapped, type #1 is mapLeft, etc)
-export interface URI2HKT<Types extends any[]> {}
+export interface URI2HKT<Types extends any[]> { }
 
 export type of<URI extends URIS> = <T>(value: T) => HKT<URI, [T, ...any]>
