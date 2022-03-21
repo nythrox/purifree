c6d25036984e6a241a8cd7051a4d141b6bf943f8
Ikechukwu Eze <92305839+ikezedev@users.noreply.github.com>
2022-03-21 12:56:04 +0100

    add more sequence test

--- src/Either.test.ts
+++ src/Either.test.ts
@@ -1,11 +1,19 @@
 import { Either, Left, Right } from './Either'
+import { NonEmptyList } from './NonEmptyList'
 import { sequenceT } from './pointfree'
 
 describe('Either', () => {
   test('sequence', () => {
-    const sequenceTTest = sequenceT(Either.of)(Right(2), Right('name'), Right(true))
+    const sequenceTTest = sequenceT(Either.of)(
+      Right(2),
+      Right('name'),
+      Right(true)
+    )
     expect(sequenceTTest).toEqual(Right([2, 'name', true]))
     expect(Left(Error()).constructor).toEqual(Either)
     expect(Right(5).constructor).toEqual(Either)
+
+    const sequenceTest2 = Right(NonEmptyList(1, 2)).sequence(NonEmptyList)
+    expect(sequenceTest2).toEqual([Right(1), Right(2)])
   })
 })
